f01b778077c5991f440da427d3f18b0a6927196c
masmoljan <m.smoljan@outlook.com>
2024-11-08 14:42:19 +0100

    added missing component

---
+++ src/components/Comment/Delete.tsx
@@ -0,0 +1,61 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle
+} from "@/components/ui/dialog";
+import { BUTTONS, TEXT } from "@/utils/constants";
+import { Button } from "../ui/button";
+
+interface DeleteProps {
+  commentId: string;
+  handleDelete: (id: string) => void;
+  open: boolean;
+  setDelete: () => void;
+}
+
+export default function Delete({ 
+  commentId, 
+  handleDelete,
+  open, 
+  setDelete
+} : DeleteProps) {
+
+  return (
+    <Dialog open={open} onOpenChange={() => setDelete()}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{TEXT.DELETE_COMMENT}</DialogTitle>
+          <DialogDescription className="text-justify pt-3 pb-3">
+            {TEXT.DELETE}
+          </DialogDescription>
+          <DialogFooter 
+            className="flex flex-row gap-2 justify-center sm:justify-center"
+          >
+            <Button 
+              className="uppercase"
+              type="submit" 
+              onClick={() => {
+                setDelete();
+              }}
+              >
+              {BUTTONS.CANCEL_DELETE}
+            </Button>
+            <Button 
+              className="uppercase"
+              type="submit" 
+              onClick={() => {
+                setDelete();
+                handleDelete(commentId);
+              }}
+            >
+              {BUTTONS.CONFIRM_DELETE}
+            </Button>
+          </DialogFooter>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
